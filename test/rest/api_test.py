97b8580f31a4372d9b17093a9175f92caf383793
pizquita <mayca.valverde@gmail.com>
2024-12-16 19:20:21 +0100

    Añade nuevos test

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -34,6 +34,25 @@ class TestApi(unittest.TestCase):
         self.assertEqual(
             response.read().decode(), "8", "ERROR SQRT"
         )
-
+        
+     def test_api_multiply(self):
+        url = f"{BASE_URL_MOCK}/calc/multiply/10/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "20", "ERROR MULTIPLY"
+        )
+        
+     def test_api_divide(self):
+        url = f"{BASE_URL_MOCK}/calc/divide/10/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "5", "ERROR MULTIPLY"
+        )
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
